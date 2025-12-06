3e6dc8f0b2f5a3f61e78d015addf91c14df94f00
Lock <unlockthecodes@gmail.com>
2025-12-06 16:14:17 +0800

    Refactor color assignment logic to use dynamic text color based on brightness

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ const button = document.getElementById('btn-main');
 button.addEventListener('click', function() {
     // Generate random RGB color
     button.style.backgroundColor = randomColor();
-    button.style.color = randomColor();
+    button.style.color = dynamicTextColor();
 });
 
+
+// Global RGB values
+let r, g, b;
 
 // Helper function to generate a random number between 0 and 255
 function randomNumber255(){
@@ -17,5 +20,18 @@ function randomNumber255(){
 
 // Make the random color generator a function to reuse 
 function randomColor(){
-    return `rgb(${randomNumber255()}, ${randomNumber255()}, ${randomNumber255()})`;
+    r = randomNumber255();
+    g = randomNumber255();
+    b = randomNumber255();
+    return `rgb(${r},${g},${b})`;
+}
+
+// Brightness Calculation function
+function brightness(r, g, b) {
+    return Math.round(((r * 299) + (g * 587) + (b * 114)) / 1000);
+}
+
+function dynamicTextColor(){
+    // return white or black if brightness, passing along rgb values is less than or greater than 128
+    return brightness(r, g, b) < 128 ? 'white' : 'black';
 }
