e8433f7b4e7313d9e138dd26d44b4b4130758fde
Lock <unlockthecodes@gmail.com>
2025-12-06 14:18:26 +0800

    Refactor random color generation into a reusable function

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ const button = document.getElementById('btn-main');
 // Listener for click event so it changes the color randomly
 button.addEventListener('click', function() {
     // Generate random RGB color
-    const randomColor = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
-    button.style.backgroundColor = randomColor;
+    button.style.backgroundColor = randomColor();
+    button.style.color = randomColor();
 });
 
+// Make the random color generator a function to reuse 
+function randomColor(){
+    return `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
+}
